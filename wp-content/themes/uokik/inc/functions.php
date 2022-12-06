0917c3f98226b0c7fa63c7ee3c6ba45b384a20ed
vakulkin <anton.vakulkin@gmail.com>
2022-12-06 11:34:08 +0100

    fixes

--- wp-content/themes/uokik/inc/functions.php
+++ wp-content/themes/uokik/inc/functions.php
@@ -4,12 +4,13 @@ function showTitle()
 {
   $the_title = get_the_title();
   $title_with_bg = get_field('title_with_bg');
+  $bg_img = get_field('bg_img');
 
-  if ($title_with_bg === 'yes') {
+  if ($title_with_bg === 'yes' && $bg_img) {
     $title_desc = get_field('title_desc');
 
     echo <<<HTML
-			<div class="titleSectionBg" style="background-image: url(http://uokik/wp-content/uploads/2022/11/image-323.png);">
+			<div class="titleSectionBg" style="background-image: url($bg_img);">
 				<div class="titleSectionBg__inner">
 					<h1 class="titleSectionBg__inner-title">$the_title</h1>
 					<div class="titleSectionBg__inner-desc">$title_desc</div>
--- wp-content/themes/uokik/src/js/script.js
+++ wp-content/themes/uokik/src/js/script.js
@@ -5,14 +5,14 @@ import "./app/scrollTo";
 
 stickyHeader();
 
-// var splide = new Splide(".splideTabsSlider", {
-//   // type: "loop",
-//   pagination: false,
-//   autoWidth: true,
-//   perPage: 5,
-//   // padding: { left: 70, right: 70 },
-// });
-// splide.mount();
+var splide = new Splide(".splideTabsSlider", {
+  // type: "loop",
+  pagination: false,
+  autoWidth: true,
+  perPage: 5,
+  // padding: { left: 70, right: 70 },
+});
+splide.mount();
 
 jQuery(".menu-toggle").on("click", function () {
   jQuery(".main-navigation ").toggleClass("toggled");
